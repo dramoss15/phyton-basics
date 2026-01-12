e1aed084217b8fc95ece4e970c1604e577ec257b
Diego Ramos <dramoscoronado@gmail.com>
2026-01-11 22:53:15 -0500

    Primer commit - Dia 1 Python

---
+++ dia1.py
@@ -0,0 +1,7 @@
+#EJERCICIO QUE PIDA:
+#NOMBRE, EDAD Y MUESTRE:"HOLA DIEGO, TIENES 22 AÑOS"
+
+nombre = input(" ¿Cómo te llamas? ")
+edad= input(" ¿Cuántos años tienes? ")
+print("Hola", nombre, ", tienes", edad, "años")
+
---
+++ ejercicios.py
@@ -0,0 +1,8 @@
+#PIDA 2 NUMEROS Y MUESTRE LA SUMA
+
+numero1 = int(input("Dame un número: "))
+numero2 = int(input("Dame otro número: "))
+
+suma = numero1 + numero2
+
+print("La suma de los 2 números es:", suma)
